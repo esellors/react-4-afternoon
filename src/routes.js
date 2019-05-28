2663e3937c0d8ebae7deb2dcc95db9d748563117
Eric <github@esellors.com>
2019-05-28 16:51:30 -0500

    customized 404 page

--- src/routes.js
+++ src/routes.js
@@ -10,5 +10,10 @@ export default <Switch>
    <Route component={About} path='/about' />
    <Route component={ClassList} path='/classlist/:class' />
    <Route component={Student} path='/student/:id' />
-   <Route render={() => <h1>404 NOT FOUND</h1>} />
+   <Route render={() => (
+      <div style={{color:'red', textAlign:'center'}}>
+         <h1>404 NOT FOUND</h1>
+         <img src="http://3.bp.blogspot.com/-ZFKCd2ldIo4/T5u93aYEDbI/AAAAAAAAFJM/UVoWuuOgW1k/s1600/funny+angry+face+(3).jpg" />
+      </div>
+   )} />
 </Switch>
