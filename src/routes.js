dca9445b652555084297c795af0d6107960880cf
Eric <github@esellors.com>
2019-05-28 16:43:38 -0500

    solutions

---
+++ src/routes.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {Switch, Route} from 'react-router-dom';
+import Home from './components/Home/Home';
+import About from './components/About/About';
+import ClassList from './components/ClassList/ClassList';
+import Student from './components/Student/Student';
+
+export default <Switch>
+   <Route component={Home} exact path='/' />
+   <Route component={About} path='/about' />
+   <Route component={ClassList} path='/classlist/:class' />
+   <Route component={Student} path='/student/:id' />
+   <Route render={() => <h1>404 NOT FOUND</h1>} />
+</Switch>
